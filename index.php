97d0d130881e4cd3abfa68c50756b0b4d917ce46
YeBeKhe <64287898+yebekhe@users.noreply.github.com>
2023-04-10 02:41:44 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,84 +1,72 @@
 <?php
-if (isset($_GET['host'])){
-    $host = $_GET['host'];
-    
-    // Set the endpoint URL
-    $url = "https://check-host.net/check-ping?host=".$host."&node=ir3.node.check-host.net&node=ir4.node.check-host.net&node=ir1.node.check-host.net";
+if (isset($_GET["host"])) {
+    $host = $_GET["host"];
 
-    // Initialize a new cURL session and set options
+    $url = "https://check-host.net/check-ping?host={$host}&node=ir3.node.check-host.net&node=ir4.node.check-host.net&node=ir1.node.check-host.net";
+
     $ch = curl_init();
-    curl_setopt_array($ch, array(
+    curl_setopt_array($ch, [
         CURLOPT_URL => $url,
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => array("Accept: application/json")
-    ));
-
-    // Execute the cURL request and get the response
+        CURLOPT_HTTPHEADER => ["Accept: application/json"],
+    ]);
+  
     $response = curl_exec($ch);
 
-    // Check for any errors
-    if(curl_errno($ch)) {
+    if (curl_errno($ch)) {
         echo "cURL error: " . curl_error($ch);
     } else {
-        // Get request id from response
-        $request_id = json_decode($response, true)['request_id'];
-      
-        // Sleep Until response created
+        $request_id = json_decode($response, true)["request_id"];
         sleep(3);
-        // Create new url , fetch and decode it's data
-        $new_url = "https://check-host.net/check-result/".$request_id;
+        $new_url = "https://check-host.net/check-result/{$request_id}";
         $new_response = file_get_contents($new_url);
-        $decoded_response = json_decode($new_response , true);
-      
-        $Tehran_ping = $Shiraz_ping = $Tabriz_ping = 0 ;
-        
-        // Checks if the Ping values are available, collects and averages the OK data and converts them to milliseconds
-        if(!empty($decoded_response["ir1.node.check-host.net"])){
-          $count = 0 ;
-    foreach($decoded_response["ir1.node.check-host.net"][0] as $value){
-        if($value[0] == "OK"){
-            $count ++ ; 
-            $Tehran_ping += $value[1];
+        $decoded_response = json_decode($new_response, true);
+
+        $Pings = [
+            "Tehran" => 0,
+            "Tabriz" => 0,
+            "Shiraz" => 0,
+        ];
+
+        if (!empty($decoded_response["ir1.node.check-host.net"])) {
+            $count = 0;
+            foreach ($decoded_response["ir1.node.check-host.net"][0] as $value) {
+                if ($value[0] == "OK") {
+                    $count++;
+                    $Pings["Tehran"] += $value[1];
+                }
+            }
+            $Pings["Tehran"] = (@$Pings["Tehran"] / $count) * 1000;
         }
+
+        if (!empty($decoded_response["ir3.node.check-host.net"])) {
+            $count = 0;
+            foreach ($decoded_response["ir3.node.check-host.net"][0] as $value) {
+                if ($value[0] == "OK") {
+                    $count++;
+                    $Pings["Shiraz"] += $value[1];
+                }
+            }
+            $Pings["Shiraz"] = (@$Pings["Shiraz"] / $count) * 1000;
+        }
+
+        if (!empty($decoded_response["ir4.node.check-host.net"])) {
+            $count = 0;
+            foreach ($decoded_response["ir4.node.check-host.net"][0] as $value) {
+                if ($value[0] == "OK") {
+                    $count++;
+                    $Pings["Tabriz"] += $value[1];
+                }
+            }
+            $Pings["Tabriz"] = (@$Pings["Tabriz"] / $count) * 1000;
+        }
+
+        $json_pings = json_encode($Pings, JSON_PRETTY_PRINT);
+
+        echo $json_pings;
     }
-          $Tehran_ping = ( @$Tehran_ping / $count) * 1000;
-        }
 
-      if(!empty($decoded_response["ir3.node.check-host.net"])){
-        $count = 0 ;
-    foreach($decoded_response["ir3.node.check-host.net"][0] as $value){
-        if($value[0] == "OK"){
-             $count ++ ; 
-             $Shiraz_ping += $value[1];
-        }
-    }
-           $Shiraz_ping = ( @$Shiraz_ping / $count) * 1000;
-        }
-
-      if(!empty($decoded_response["ir4.node.check-host.net"])){
-        $count = 0 ;
-    foreach($decoded_response["ir4.node.check-host.net"][0] as $value){
-        if($value[0] == "OK"){
-             $count ++ ; 
-             $Tabriz_ping += $value[1];
-        }
-    }
-           $Tabriz_ping = ( @$Tabriz_ping / $count) * 1000;
-        }
-
-
-        $Pings = ["Tehran" => $Tehran_ping , "Tabriz" => $Tabriz_ping , "Shiraz" => $Shiraz_ping];
-
-        $json_pings = json_encode($Pings , JSON_PRETTY_PRINT);
-
-         echo $json_pings;
-        
-    }
-
-    // Close the cURL session
     curl_close($ch);
-
 } else {
     echo "Please provide a host and retry!";
 }
-?>
